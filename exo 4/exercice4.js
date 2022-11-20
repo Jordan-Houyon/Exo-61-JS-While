cacd77e80ffb7f90a922bcfdc2ccc0a547d47ed5
jordan houyon <116800565+Jordan-Houyon@users.noreply.github.com>
2022-11-20 21:38:04 +0100

    exo 61 fini

--- exo 4/exercice4.js
+++ exo 4/exercice4.js
@@ -1,9 +1,10 @@
 /* Modifier le code ci dessous pour réaliser l'exercice */
 
 var i = 15;
-while (i   ) {
+while (i <35  ) {
     document.getElementById('monDiv').innerHTML+="Ma variable i vaux "+i;
-    =   ;
+  i+=5;
+  document.write(i);
 }
 
 
